efd62f21386b612420fae69e99977ff46ede3d15
unknown <lorenzofattizzo19@gmail.com>
2019-12-05 17:50:50 +0100

    Aggiungiamo file HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+
+public class HelloWorld {
+
+    public static void main(String[] args) {	
+	    System.out.println("Hello World!");	
+		System.out.println("Ciao");
+		System.out.println("Ragazzi");
+		System.out.print("Ciao "+"Ragazzi" );
+		System.out.print("Siamo in ");
+		System.out.println(12+4);
+		System.out.println("12"+" 4");	
+		}
+		
+}
